d83b0d7dca4f5084e6aa0a105e1a83076abf2b5d
Miku <mikulej7@gmail.com>
2026-03-29 16:10:26 +0200

    basic display

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
+import pygame
 
 def main():
+    
     print("Hello World!")
-    return 0
+
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    pygame.display.set_caption("Zelda Bomber")
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+
+    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("lightblue")
+
+        # ---- Render ----
+
+        pygame.draw.circle(screen, "red", player_pos, 40)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            player_pos.y -= 300 * dt
+        if keys[pygame.K_s]:
+            player_pos.y += 300 * dt
+        if keys[pygame.K_a]:
+            player_pos.x -= 300 * dt
+        if keys[pygame.K_d]:
+            player_pos.x += 300 * dt
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
 
 if __name__=="__main__":
     main()
