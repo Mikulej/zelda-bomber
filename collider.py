1c2904519e5077639e5a1a9947ff25f9ef34483b
Miku <mikulej7@gmail.com>
2026-05-08 20:16:54 +0200

    pass color to collider constructor

--- collider.py
+++ collider.py
@@ -7,7 +7,7 @@ class Collider(GameObject):
   collider_id_list = [] 
   
   def __init__(self, x: float, y: float, width: float, height: float, color: pygame.Color = (255,255,255), center: bool = False):
-    super().__init__(x, y, width, height)
+    super().__init__(x, y, width, height, color)
     Collider.collider_id_list.append(self.id)
 
   def __del__(self):
