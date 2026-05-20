83f0559db0234acf09a0a2f0c1d577737c345fa5
Miku <mikulej7@gmail.com>
2026-05-20 10:35:36 +0200

    add docstrings to GameObject class

--- game_object.py
+++ game_object.py
@@ -4,9 +4,29 @@ from render import Renderer
 GAME_OBJECT_LIMIT = 100
 
 class GameObject():
-    initialized = False
-    game_object_list = [] 
+    """
+    A visible rectangle on the screen. The most atomic object of a game.
+
+    Must be initialized before being used.
+
+    Attributes:
+        initialized (bool): Holds true if class was initialized
+        game_object_list (list): List of all GameObject instances
+    """
+    initialized: bool = False
+    game_object_list: list = [] 
     def __init__(self,x: float,y: float , width: float, height: float, color: pygame.Color = (255,255,255), center: bool = False):
+        """
+        Create a new GameObject.
+
+        Parameters:
+            x (float): X position of rectangle.
+            y (float): Y position of rectangle.
+            width (float): Width of rectangle.
+            height (float): Height of rectangle.
+            color (pygame.Color): Color of rectangle.
+            center (bool): If True x, y will denote center of a rectangle otherwise they denote upper-left corner.
+        """
         self.x = x
         self.y = y
         self.width = width
@@ -29,10 +49,20 @@ class GameObject():
             GameObject.game_object_list[self.id] = self
 
     def __del__(self):
+        """
+        Delete a GameObject.
+        
+        Marks an object as inactive by setting it's id to -1
+        """
         GameObject.game_object_list[self.id].id = -1
 
     @staticmethod
     def initialize():
+        """
+        Initialize a GameObject class.
+
+        Sets initialized flag to True
+        """
         for i in range(0,GAME_OBJECT_LIMIT):
             go = GameObject(0,0,0,0)
             go.id = -1
@@ -42,6 +72,9 @@ class GameObject():
 
 
     def draw(self):
+        """
+        Draw a recangle using x, y as a upper-left corner.
+        """
         pygame.draw.rect(Renderer.screen, self.color,
                          rect=pygame.Rect(self.x*Renderer.ratio.x,
                                           self.y*Renderer.ratio.y,
@@ -49,6 +82,9 @@ class GameObject():
                                           self.height*Renderer.ratio.y))
         
     def draw_center(self):
+        """
+        Draw a recangle using x, y as a center.
+        """
         pygame.draw.rect(Renderer.screen, self.color,
                          rect=pygame.Rect(self.x*Renderer.ratio.x - (self.width*Renderer.ratio.x/2),
                                           self.y*Renderer.ratio.y - (self.height*Renderer.ratio.y/2),
@@ -57,11 +93,20 @@ class GameObject():
         
     @staticmethod
     def draw_all():
+        """
+        Draw all GameObjects in game_object_list that are active.
+        """
         for go in GameObject.game_object_list:
             if go.id != -1:
                 go.draw_function(go)
 
     def serialize(self) -> dict:
+        """
+        Serialize an GameObject to dict.
+
+        Returns:
+            dict: Serialized GameObject
+        """
         data: dict = {}
         data["x"] = self.x
         data["y"] = self.y
@@ -74,6 +119,12 @@ class GameObject():
     
     @staticmethod
     def serialize_all() -> dict:
+        """
+        Serialize all GameObjects in game_object_list that are active.
+
+        Returns:
+            dict: Serialized GameObjects
+        """
         data: dict = {}
         index = 0
         for go in GameObject.game_object_list:
@@ -84,14 +135,23 @@ class GameObject():
     
     @staticmethod
     def deserialize(data: dict):
+        """
+        Deserialize a single GameObject from dict.
+        """
         GameObject(data["x"],data["y"],data["width"],data["height"],data["color"],data["center"])
     
     @staticmethod
     def deserialize_all(data: dict):
+        """
+        Deserialize multiple GameObjects from dict.
+        """
         for index in data:
             GameObject.deserialize(data[index])
             
     @staticmethod
     def destroy_all():
+        """
+        Destroy all GameObjects in game_object_list by setting their ids to -1.
+        """
         for go in GameObject.game_object_list:
             go.id = -1
